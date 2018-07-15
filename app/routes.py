5bb9df25172a1e5627900b9a50e8605f8694fb67
Paul E Carpenter <paulecar@mac.com>
2018-07-15 19:49:25 -0400

    File upload working with PDF filter and upload/session directory

--- app/routes.py
+++ app/routes.py
@@ -4,9 +4,15 @@ from werkzeug.utils import secure_filename
 from flask import render_template, flash, redirect, url_for, request, session
 
 import uuid
+import os
 
 def unique_id():
     return hex(uuid.uuid4().time)[2:-1]
+
+def allowed_file(filename):
+    return '.' in filename and \
+           filename.rsplit('.', 1)[1].lower() in app.config['ALLOWED_EXTENSIONS']
+
 
 @app.route('/')
 @app.route('/index')
@@ -28,8 +34,21 @@ def upload():
 @app.route('/uploader', methods=['GET', 'POST'])
 def uploader():
     if request.method == 'POST':
+        # check if the post request has the file part
+        if 'file' not in request.files:
+            flash('No file part')
+            return redirect(url_for('index'))
+
         f = request.files['file']
-        f.save(secure_filename(f.filename))
+
+        if f.filename == '':
+            flash('No selected file')
+            return redirect(request.url)
+        if not allowed_file(f.filename):
+            flash('Invalid file type')
+            return redirect(url_for('index'))
+        os.makedirs(os.path.join(app.config['UPLOAD_FOLDER'], session.get('ID')), exist_ok=True)
+        filename = secure_filename(f.filename)
+        f.save(os.path.join(app.config['UPLOAD_FOLDER'], session.get('ID'), filename))
         flash('file uploaded successfully')
         return redirect(url_for('index'))
-
--- config.py
+++ config.py
@@ -5,3 +5,5 @@ basedir = os.path.abspath(os.path.dirname(__file__))
 class Config(object):
     # Key for form management and combat XSS/CSRF
     SECRET_KEY = os.environ.get('SECRET_KEY') or 'hilton_head_island'
+    UPLOAD_FOLDER = './uploads'
+    ALLOWED_EXTENSIONS = set(['pdf'])
