29088b6aa52567aceb64000604d653764318a5ce
Paul E Carpenter <paulecar@mac.com>
2018-07-16 07:42:48 -0400

    Basic workflow complete and scanned rosters formatted into tables on home page

--- app/routes.py
+++ app/routes.py
@@ -2,16 +2,37 @@ from app import app
 from app.forms import UploadForm
 from werkzeug.utils import secure_filename
 from flask import render_template, flash, redirect, url_for, request, session
+from PIL import Image
+
 
 import uuid
 import os
+import locale, ghostscript
+import scantools
+import time
+
 
 def unique_id():
     return hex(uuid.uuid4().time)[2:-1]
 
+
 def allowed_file(filename):
     return '.' in filename and \
            filename.rsplit('.', 1)[1].lower() in app.config['ALLOWED_EXTENSIONS']
+
+
+def pdf2png(pdf_input_path, dpi, output_path):
+    args = ["pdf2png", # actual value doesn't matter
+            "-dNOPAUSE",
+            "-sDEVICE=png16m",
+            dpi,
+            "-sOutputFile=" + output_path,
+            pdf_input_path]
+    # arguments have to be bytes, encode them
+    encoding = locale.getpreferredencoding()
+    args = [a.encode(encoding) for a in args]
+
+    ghostscript.Ghostscript(*args)
 
 
 @app.route('/')
@@ -23,7 +44,39 @@ def index():
         session['ID'] = unique_id()
         flash('Created new cookie ' + session.get('ID'))
 
-    return render_template('index.html', title='Home')
+    hometeam = ()
+    awayteam = ()
+    homeTeamName = ""
+    awayTeamName = ""
+
+    if session.get('scan_result'):
+        flash('Using existing scan result ' + session.get('ID'))
+        lines = session.get('scan_result').splitlines()
+        teams = scantools.findTeams(lines)
+        homeTeamName = teams[0]
+        awayTeamName = teams[1]
+
+        players = scantools.findPlayers(lines)
+        # for player in players:
+        #    print("player :", player)
+
+        fixedPlayers = scantools.fixNames(players)
+        # for fplayer in fixedPlayers:
+        #    print("fplayer: ", fplayer)
+
+        # invertedPlayers = invertPlayers(fixedPlayers)
+        # print(invertedPlayers)
+        hometeam = scantools.createRoster(fixedPlayers, 0, 1, 2, 3, 4)
+        # print(homeTeamName)
+        # for p in hometeam.items():
+        #    print("hometeam :", p)
+
+        awayteam = scantools.createRoster(fixedPlayers, 5, 6, 7, 8, 9)
+        # print(awayTeamName)
+        # for p in awayteam.items():
+        #    print("awayteam: ", p)
+
+    return render_template('index.html', title='Home', teams=fixedPlayers, hometeam=hometeam, awayteam=awayteam, homename=homeTeamName, awayname=awayTeamName)
 
 
 @app.route('/upload')
@@ -53,9 +106,38 @@ def uploader():
         flash('file uploaded successfully')
         return redirect(url_for('index'))
 
-@app.route('/files', methods=['GET'])
 
+@app.route('/files', methods=['GET'])
 def files():
     f_list = os.listdir(os.path.join(app.config['UPLOAD_FOLDER'], session.get('ID')))
-
     return render_template('file_list.html',files=f_list,currdir=app.config['UPLOAD_FOLDER'] + "/" + session.get('ID'))
+
+
+@app.route('/convert/<filename>', methods=['GET'])
+def convert(filename):
+    convert_file = os.path.join(app.config['UPLOAD_FOLDER'], session.get('ID'), filename)
+    newname = filename.rsplit('.', 1)[0] + '.png'
+    png_file = os.path.join(app.config['UPLOAD_FOLDER'], session.get('ID'), newname)
+
+    print("Conv Start: ", time.strftime('%X %x %Z'))
+    pdf2png(convert_file, "-r300", png_file)
+    print("Conv End: ", time.strftime('%X %x %Z'))
+
+    f_list = os.listdir(os.path.join(app.config['UPLOAD_FOLDER'], session.get('ID')))
+    return render_template('file_list.html',files=f_list,currdir=app.config['UPLOAD_FOLDER'] + "/" + session.get('ID'))
+
+
+@app.route('/scanpng/<filename>', methods=['GET'])
+def scanpng(filename):
+    scan_file = os.path.join(app.config['UPLOAD_FOLDER'], session.get('ID'), filename)
+
+    print("Scan Start: ", time.strftime('%X %x %Z'))
+    result = scantools.scan(filename, Image.open(scan_file))
+    print("Scan End: ", time.strftime('%X %x %Z'))
+
+    flash('Scanners:')
+    flash(result)
+
+    session['scan_result'] = result
+
+    return redirect(url_for('index'))
--- scantools.py
+++ scantools.py
@@ -1,5 +1,3 @@
-import time
-from PIL import Image
 from tesserocr import PyTessBaseAPI
 
 def scan(title, image):
@@ -15,10 +13,11 @@ def scan(title, image):
 
 def findTeams(lines):
     for idx, line in enumerate(lines):
-        if "Home Team" in line:
-            print("might work:", idx)
+        # print("line", idx, line)
+        # if "Home Team" in line:
+        #    print("might work:", idx)
         if "Team:" in line:
-            print("Found Teams:", type(line), idx, line)
+        #    print("Found Teams:", type(line), idx, line)
             break
     splitline = line.split("Team:")
     teams = [t.strip() for t in splitline]
@@ -42,6 +41,7 @@ def findPlayers(lines):
             cleanline.append([i for i in dirtyline if i not in ('*','N','$25.00','$15.00')])
     return cleanline
 
+
 def invertPlayers(pairs):
     invertedlist = list(map(list, zip(*pairs)))
     return invertedlist
@@ -50,8 +50,8 @@ def invertPlayers(pairs):
 def fixNames(players):
     cleanPlayers = []
     for pair in players:
-        # 10 means wehave complete row with two players
-        print(len(pair), pair)
+        # 10 means we have complete row with two players
+        # print(len(pair), pair)
         if len(pair) == 10:
             cleanPlayers.append([col.rstrip(',') for col in pair])
         # Less than 10 then we assume one column has "last,first' concat'ed
@@ -80,38 +80,3 @@ def createRoster(teamlists, sl, mp, id, last, first):
         except IndexError:
             print("Failure in row: ", row )
     return roster
-
-
-print("Start: ", time.strftime('%X %x %Z'))
-
-ocrOutput = scan("File", Image.open("02-04-2018_scoresheet300.png"))
-# ocrOutput = scan("File", Image.open("01-08-2018_scoresheet300.png"))
-lines = ocrOutput.splitlines()
-print("Scanned: ", time.strftime('%X %x %Z'))
-
-teams = findTeams(lines)
-homeTeamName = teams[0]
-awayTeamName = teams[1]
-print("teams: ", teams)
-
-players = findPlayers(lines)
-for player in players:
-   print("player :", player)
-
-fixedPlayers = fixNames(players)
-for fplayer in fixedPlayers:
-   print("fplayer: ", fplayer)
-
-# invertedPlayers = invertPlayers(fixedPlayers)
-# print(invertedPlayers)
-homeTeam = createRoster(fixedPlayers,0,1,2,3,4)
-print(homeTeamName)
-for p in homeTeam.items():
-    print("hometeam :", p)
-
-awayTeam = createRoster(fixedPlayers,5,6,7,8,9)
-print(awayTeamName)
-for p in awayTeam.items():
-    print("awayteam: ", p)
-
-print("Done: ", time.strftime('%X %x %Z'))
